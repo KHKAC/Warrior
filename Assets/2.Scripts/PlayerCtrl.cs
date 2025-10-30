6ab54652319e404a9fe84f1f83da23c28366eed8
KHKAC <rudgnsrb888@naver.com>
2025-10-30 14:01:07 +0900

    251030 Input System

---
+++ Assets/2.Scripts/PlayerCtrl.cs
@@ -0,0 +1,64 @@
+#pragma warning disable IDE0051
+
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerCtrl : MonoBehaviour
+{
+    Animator anim;
+    new Transform transform;
+    Vector3 moveDir;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        transform = GetComponent<Transform>();
+    }
+
+
+    void Update()
+    {
+        if (moveDir != Vector3.zero)
+        {
+            transform.rotation = Quaternion.LookRotation(moveDir);
+            transform.Translate(Vector3.forward * Time.deltaTime * 4.0f);
+        }
+    }
+
+    #region SEND_MESSAGE
+    void OnMove(InputValue value)
+    {
+        Vector2 dir = value.Get<Vector2>();
+        moveDir = new Vector3(dir.x, 0, dir.y);
+        anim.SetFloat("Movement", dir.magnitude);
+        Debug.Log($"Move = ({dir.x}, {dir.y})");
+    }
+
+    void OnAttack()
+    {
+        Debug.Log("Attack");
+        anim.SetTrigger("Attack");
+    }
+    #endregion
+
+    #region UNITY_EVENTS
+    public void OnMove(InputAction.CallbackContext ctx)
+    {
+        Vector2 dir = ctx.ReadValue<Vector2>();
+        moveDir = new Vector3(dir.x, 0, dir.y);
+        anim.SetFloat("Movement", dir.magnitude);
+    }
+
+    public void OnAttack(InputAction.CallbackContext ctx)
+    {
+        Debug.Log($"ctx.phase = {ctx.phase}");
+
+        if(ctx.performed)
+        {
+            Debug.Log("Attack");
+            anim.SetTrigger("Attack");
+        }
+    }
+    #endregion
+
+}
